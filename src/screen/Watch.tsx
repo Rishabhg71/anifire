518ed9a35456ddce074ee26e3a97bc2bc8e03fba
RG <rishabhgautam727@gmail.com>
2023-02-24 11:40:40 +0530

    added videojs | added HLS stream view

--- src/screen/Watch.tsx
+++ src/screen/Watch.tsx
@@ -1,34 +1,107 @@
-// import React from "react";
-// import { IEAnime } from "../types";
-// import { useNavigate } from "react-router-dom";
-// const Card = ({ animeInfo }: { animeInfo: IEAnime }) => {
-//   const navigate = useNavigate();
-//   return (
-//     <div
-//       className="w-64 h-120 rounded-md m-2 text-cyan-50"
-//       style={{ backgroundColor: "#2a2c31" }}
-//       onClick={() => {
-//         console.log("CLICKED ON CARD");
-//         navigate(`/episode/${animeInfo.animeId}`);
-//       }}
-//     >
-//       <img
-//         className="w-full h-80"
-//         // style={{ backgroundImage:  }}
-//         src={animeInfo.animeImg}
-//       ></img>
-//       <div className="flex justify-center ">{animeInfo.animeTitle}</div>
-//     </div>
-//   );
-// };
-
 import React from "react";
 import { IEAnime } from "../types";
 import { useNavigate, useParams } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import LoadingIcon from "../components/Loading";
 
-const WatchEpisode = ({ animeInfo }: { animeInfo: IEAnime }) => {
+import videojs from "video.js";
+import "video.js/dist/video-js.css";
+
+const WatchEpisode = () => {
   const param = useParams();
-  return <div className="text-gray-200">{param.id}</div>;
+  const { data, isLoading } = useQuery({
+    queryFn: () =>
+      axios
+        .get(`https://gogoanime.consumet.stream/vidcdn/watch/${param.id}`)
+        .then((d) => d.data),
+    queryKey: ["WATCH_EPISODE"],
+  });
+
+  const playerRef = React.useRef(null);
+
+  const handlePlayerReady = (player: any) => {
+    playerRef.current = player;
+
+    // You can handle player events here, for example:
+    player.on("waiting", () => {
+      videojs.log("player is waiting");
+    });
+
+    player.on("dispose", () => {
+      videojs.log("player will dispose");
+    });
+  };
+
+  if (isLoading) return <LoadingIcon />;
+  console.log(data);
+
+  const videoJsOptions = {
+    autoplay: true,
+    controls: true,
+    responsive: true,
+    fluid: true,
+    sources: [
+      {
+        src: data.sources[0].file,
+        type: "application/x-mpegURL",
+      },
+    ],
+  };
+
+  return (
+    <div className="text-gray-200">
+      <VideoJS options={videoJsOptions} onReady={handlePlayerReady} />
+    </div>
+  );
+};
+
+export const VideoJS = (props: any) => {
+  const videoRef = React.useRef<any>(null);
+  const playerRef = React.useRef<any>(null);
+  const { options, onReady } = props;
+
+  React.useEffect(() => {
+    // Make sure Video.js player is only initialized once
+    if (!playerRef.current) {
+      // The Video.js player needs to be _inside_ the component el for React 18 Strict Mode.
+      const videoElement = document.createElement("video-js");
+
+      videoElement.classList.add("vjs-big-play-centered");
+      videoRef.current?.appendChild(videoElement);
+
+      const player = (playerRef.current = videojs(videoElement, options, () => {
+        videojs.log("player is ready");
+        onReady && onReady(player);
+      }));
+
+      // You could update an existing player in the `else` block here
+      // on prop change, for example:
+    } else {
+      const player = playerRef.current;
+
+      player.autoplay(options.autoplay);
+      player.src(options.sources);
+    }
+  }, [options, videoRef]);
+
+  // Dispose the Video.js player when the functional component unmounts
+  React.useEffect(() => {
+    const player = playerRef.current;
+
+    return () => {
+      if (player && !player.isDisposed()) {
+        player.dispose();
+        playerRef.current = null;
+      }
+    };
+  }, [playerRef]);
+
+  return (
+    <div data-vjs-player>
+      <div ref={videoRef} />
+    </div>
+  );
 };
 
 export default WatchEpisode;
