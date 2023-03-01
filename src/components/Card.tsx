040e80909447fca4b7e99eb8b6e5bca14386168d
RG7279805 <47270636+RG7279805@users.noreply.github.com>
2023-03-01 17:30:40 +0530

    added new video player for HLS video (#2)
    * added Video Player
    
    * api updated to 9anime
    
    * removed videojs

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -23,7 +23,7 @@
 // };
 
 import React from "react";
-import { IEAnime } from "../types";
+import { IEAnime, IAnimeDetail, IEResult } from "../types";
 import { useNavigate } from "react-router-dom";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
@@ -31,26 +31,31 @@ import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import { CardActionArea } from "@mui/material";
 
-const AnimeCard = ({ animeInfo }: { animeInfo: IEAnime }) => {
+const AnimeCard = ({ animeInfo }: { animeInfo: IEResult }) => {
   const navigate = useNavigate();
   return (
     <Card
-      sx={{ maxWidth: 345 }}
-      className="w-72 m-2"
+      style={{ backgroundColor: "ActiveBorder" }}
+      className="w-72 m-2 h-140 hover:border-zinc-50 hover:border-4 focus:border-zinc-50 focus:border-4"
       onClick={() => {
-        navigate(`/episode/${animeInfo.animeId}`);
+        navigate(`/episode/${animeInfo.id}`);
       }}
     >
-      <CardActionArea>
+      <CardActionArea className="bg-slate-600 ">
         <CardMedia
           component="img"
-          height="140"
-          image={animeInfo.animeImg}
+          // height="140"
+          image={animeInfo.image}
           alt="green iguana"
         />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            {animeInfo.animeTitle}
+        <CardContent className="">
+          <Typography
+            gutterBottom
+            variant="h5"
+            component="div"
+            className="text-gray-50 text-center"
+          >
+            {animeInfo.title}
           </Typography>
         </CardContent>
       </CardActionArea>
--- src/screen/Watch.tsx
+++ src/screen/Watch.tsx
@@ -1,105 +1,28 @@
 import React from "react";
-import { IEAnime } from "../types";
+import { IEAnime, IWatchEpisode } from "../types";
 import { useNavigate, useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import LoadingIcon from "../components/Loading";
-
-import videojs from "video.js";
-import "video.js/dist/video-js.css";
+import api from "../services/axios";
+import ReactPlayer from "react-player";
 
 const WatchEpisode = () => {
   const param = useParams();
-  const { data, isLoading } = useQuery({
-    queryFn: () =>
-      axios
-        .get(`https://gogoanime.consumet.stream/vidcdn/watch/${param.id}`)
-        .then((d) => d.data),
-    queryKey: ["WATCH_EPISODE"],
+  const { data, isLoading } = useQuery<IWatchEpisode>({
+    queryFn: () => api.get(`/watch/${param.id}`).then((d) => d.data),
+    queryKey: ["WATCH_EPISODE", param.id],
   });
 
-  const playerRef = React.useRef(null);
-
-  const handlePlayerReady = (player: any) => {
-    playerRef.current = player;
-
-    // You can handle player events here, for example:
-    player.on("waiting", () => {
-      videojs.log("player is waiting");
-    });
-
-    player.on("dispose", () => {
-      videojs.log("player will dispose");
-    });
-  };
-
-  if (isLoading) return <LoadingIcon />;
-  console.log(data);
-
-  const videoJsOptions = {
-    autoplay: true,
-    controls: true,
-    responsive: true,
-    fluid: true,
-    sources: [
-      {
-        src: data.sources[0].file,
-        type: "application/x-mpegURL",
-      },
-    ],
-  };
-
   return (
-    <div className="text-gray-200">
-      <VideoJS options={videoJsOptions} onReady={handlePlayerReady} />
-    </div>
-  );
-};
-
-export const VideoJS = (props: any) => {
-  const videoRef = React.useRef<any>(null);
-  const playerRef = React.useRef<any>(null);
-  const { options, onReady } = props;
-
-  React.useEffect(() => {
-    // Make sure Video.js player is only initialized once
-    if (!playerRef.current) {
-      // The Video.js player needs to be _inside_ the component el for React 18 Strict Mode.
-      const videoElement = document.createElement("video-js");
-
-      videoElement.classList.add("vjs-big-play-centered");
-      videoRef.current?.appendChild(videoElement);
-
-      const player = (playerRef.current = videojs(videoElement, options, () => {
-        videojs.log("player is ready");
-        onReady && onReady(player);
-      }));
-
-      // You could update an existing player in the `else` block here
-      // on prop change, for example:
-    } else {
-      const player = playerRef.current;
-
-      player.autoplay(options.autoplay);
-      player.src(options.sources);
-    }
-  }, [options, videoRef]);
-
-  // Dispose the Video.js player when the functional component unmounts
-  React.useEffect(() => {
-    const player = playerRef.current;
-
-    return () => {
-      if (player && !player.isDisposed()) {
-        player.dispose();
-        playerRef.current = null;
-      }
-    };
-  }, [playerRef]);
-
-  return (
-    <div data-vjs-player>
-      <div ref={videoRef} />
+    <div className="align-middle flex justify-center">
+      <ReactPlayer
+        // style={{ width: "90%", height: "120" }}
+        height="50em"
+        width="90%"
+        url={data?.sources[0].url}
+        controls={true}
+      />
     </div>
   );
 };
