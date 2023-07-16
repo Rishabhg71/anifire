6e2344ff5476a9c1fca284d2f937898bb87dd398
Rishabh <rishabhgautam727@gmail.com>
2023-07-16 20:07:01 +0530

    [ADD] Back button event on video page

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -12,8 +12,12 @@ import Hls from "hls.js";
 const WatchEpisode = ({ episodeId }: { episodeId: string }) => {
   //   const param = useParams();
   const ref = useRef<HTMLVideoElement>(null);
+  const videoContainer = useRef<HTMLDivElement>(null);
   const { data, isLoading } = useQuery<IWatchEpisode>({
-    queryFn: () => api.get(`/watch/${episodeId}`).then((d) => d.data),
+    queryFn: () =>
+      episodeId
+        ? api.get(`/watch/${episodeId}`).then((d) => d.data)
+        : new Promise((resolve) => resolve(true)).then(() => []),
     queryKey: ["WATCH_EPISODE", episodeId],
   });
   const remote = useContext(ControlsContext);
@@ -23,21 +27,51 @@ const WatchEpisode = ({ episodeId }: { episodeId: string }) => {
   //       if (e === "play-pause") ref.current?.play();
   //     }, "WATCH_PAGE");
   //   }, []);
-
+  useEffect(() => {
+    remote.registerEvent((e) => {
+      // ref.current?.setAttribute("hidden", "true");
+      ref.current?.pause();
+      // videoContainer.current?.appendChild(document.createElement("video"));
+    }, "BACK_REMOVE_VIDEO_ELEMENT");
+  }, []);
   useEffect(() => {
     const hls = new Hls();
     if (data && ref.current) {
-      hls.loadSource(data?.sources[0].url);
+      const sourcesSorted: { [key: string]: string } = {};
+      data?.sources.forEach((el) => {
+        sourcesSorted[el.quality] = el.url;
+      });
+      let loadSource = "";
+      if (sourcesSorted["360p"]) {
+        loadSource = sourcesSorted["360p"];
+      }
+      if (sourcesSorted["480p"]) {
+        loadSource = sourcesSorted["480p"];
+      }
+      if (sourcesSorted["720p"]) {
+        loadSource = sourcesSorted["720p"];
+      }
+      if (sourcesSorted["1080p"]) {
+        loadSource = sourcesSorted["1080p"];
+      }
+      console.log(loadSource);
+      hls.loadSource(loadSource);
       hls.attachMedia(ref.current);
       ref.current.play();
       ref.current.requestFullscreen();
+      // addEventListener("fullscreenchange", (event) => {
+      //   console.log(event.);
+      // });
     }
   }, [data?.sources]);
 
   if (isLoading) return <LoadingIcon />;
 
   return (
-    <div className="align-middle flex justify-center flex-col text-white">
+    <div
+      className="align-middle flex justify-center flex-col text-white"
+      ref={videoContainer}
+    >
       <video ref={ref} controls={false} />
     </div>
   );
